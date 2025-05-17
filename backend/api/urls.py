ab8adec22b3175c1303dcffd4d42e26ced738d02
skander Ben achour <benachour.skander1@gmail.com>
2025-05-17 12:48:43 +0100

    adding note create view and delete view and urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,11 +2,19 @@
 
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import CreateUserView
+from .views import CreateUserView, NoteListCreate, NoteDelete
 
-urlpatterns = [
+auth_urls = [
     path("api/user/register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls"))
 ]
+
+note_urls=[
+    path("notes/", NoteListCreate.as_view(), name="note-list"),
+    path("notes/delete/<int:pk>/", NoteDelete.as_view(), name="delete-note"),
+    
+]
+
+urlpatterns = auth_urls + note_urls
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+
+from .models import Note
+from .serializers import NoteSerializer, UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
@@ -10,3 +12,26 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+    
+
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
+    def perform_create(self, serializer=NoteSerializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
