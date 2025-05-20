c0eae6f3cf95680ddbea2f93fa51a61b03e2e5e5
skander Ben achour <benachour.skander1@gmail.com>
2025-05-20 13:58:31 +0100

    added home and notes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import api from '../api';
+import Note from "../components/Note";
+import '../styles/Home.css';
 
 
 
@@ -21,12 +23,64 @@ function Home  () {
     
   } 
 
+  const createNote = (e) => {
+    e.preventDefault();
+    api
+        .post("/api/notes/", { content, title })
+        .then((res) => {
+            if (res.status === 201) alert("Note created!");
+            else alert("Failed to make note.");
+            getNotes();
+        })
+        .catch((err) => alert(err));
+};
 
-  return (
-    <div>
-      Home
-    </div>
-  )
+  
+  const deleteNote = (id) => {
+    api
+        .delete(`/api/notes/delete/${id}/`)
+        .then((res) => {
+            if (res.status === 204) alert("Note deleted!");
+            else alert("Failed to delete note.");
+            getNotes();
+        })
+        .catch((error) => alert(error));
+};
+
+return (
+  <div>
+      <div>
+          <h2>Notes</h2>
+          {notes.map((note) => (
+              <Note note={note} onDelete={deleteNote} key={note.id} />
+          ))}
+      </div>
+      <h2>Create a Note</h2>
+      <form onSubmit={createNote}>
+          <label htmlFor="title">Title:</label>
+          <br />
+          <input
+              type="text"
+              id="title"
+              name="title"
+              required
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+          />
+          <label htmlFor="content">Content:</label>
+          <br />
+          <textarea
+              id="content"
+              name="content"
+              required
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+          ></textarea>
+          <br />
+          <input type="submit" value="Submit"></input>
+      </form>
+  </div>
+);
 }
 
 export default Home
